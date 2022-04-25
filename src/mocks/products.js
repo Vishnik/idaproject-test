32b5a6db7c9900626435fbc0d993e5ed710121f4
Vishnik <Taras.Vishnik@enterra-inc.com>
2022-04-26 01:50:35 +0200

    add more items to products mock

--- src/mocks/products.js
+++ src/mocks/products.js
@@ -10,7 +10,7 @@ const products = [
   {
     id: '2',
     title: 'Наименование товара',
-    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
     image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
     price: 10000,
   },
@@ -26,13 +26,100 @@ const products = [
   {
     id: '4',
     title: 'Наименование товара',
-    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
     image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
     price: 10000,
   },
 
   {
     id: '5',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '6',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '7',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '8',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '9',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '10',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+  {
+    id: '11',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '12',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '13',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '14',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '15',
+    title: 'Наименование товара',
+    description: 'Довольно-таки интересное описание товара в несколько строк.',
+    image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
+    price: 10000,
+  },
+
+  {
+    id: '11',
     title: 'Наименование товара',
     description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
     image: 'https://traum-deutung.de/wp-content/uploads/2017/08/traum-fotoapparat-deutung-symbol.jpg',
