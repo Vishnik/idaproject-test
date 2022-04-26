d0a2d086e48f2da11692fee4f11bdc5554dcdb90
Vishnik <Taras.Vishnik@enterra-inc.com>
2022-04-26 03:10:24 +0200

    add sorting to ProductListContainer

--- src/components/containers/ProductListContainer.vue
+++ src/components/containers/ProductListContainer.vue
@@ -1,22 +1,48 @@
 <template>
-  <product-list @delete-product="onDeleteProduct" :products="products"></product-list>
+  <div class="product-list-container">
+    <header class="product-list-container__header">
+      <div class="product-list-container__sort-select">
+        <v-select :options="sortOptions" v-model="currentSortValue"></v-select>
+      </div>
+    </header>
+    <product-list @delete-product="onDeleteProduct" :products="sortedProducts"></product-list>
+  </div>
 </template>
 
 <script>
-import { computed, onBeforeMount } from 'vue';
+import { computed, ref, onBeforeMount } from 'vue';
 import { useStore } from 'vuex';
+import ProductSortOptions from '../../utils/ProductSortOptions';
 
 import ProductList from '../ProductList.vue';
+import VSelect from '../ui/VSelect.vue';
 
 export default {
   name: 'ProductListContainer',
   components: {
     ProductList,
+    VSelect,
   },
 
   setup() {
     const store = useStore();
     const products = computed(() => store.getters['ProductStore/products']);
+    const sortOptions = [
+      {
+        label: 'По умолчанию',
+        value: ProductSortOptions.NOT_SORTING,
+      },
+      {
+        label: 'По цене min',
+        value: ProductSortOptions.PRICE_ASC,
+      },
+      {
+        label: 'По цене max',
+        value: ProductSortOptions.PRICE_DEC,
+      },
+    ];
+
+    const currentSortValue = ref(sortOptions[0].value);
 
     onBeforeMount(() => {
       if (!products.value.length) {
@@ -28,14 +54,42 @@ export default {
       store.dispatch('ProductStore/deleteProduct', productId);
     };
 
+    const sortedProducts = computed(() => {
+      switch (currentSortValue.value) {
+        case ProductSortOptions.PRICE_DEC: return store.getters['ProductStore/sortedByDecPriceProducts'];
+        case ProductSortOptions.PRICE_ASC: return store.getters['ProductStore/sortedByAscPriceProducts'];
+        case ProductSortOptions.NOT_SORTING: return products.value;
+        default: return products.value;
+      }
+    });
+
     return {
       products: computed(() => store.getters['ProductStore/products']),
       onDeleteProduct,
+      sortOptions,
+      currentSortValue,
+      sortedProducts,
     };
   },
 };
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.product-list-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: flex-start;
+  &__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  &__sort-select {
+    width: 121.48px;
+    margin-bottom: 16px;
+  }
+}
 </style>
