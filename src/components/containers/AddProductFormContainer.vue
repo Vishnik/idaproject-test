9605f21f6d2d8102901739c35f7f0f9f0c6ec049
Vishnik <Taras.Vishnik@enterra-inc.com>
2022-04-26 10:48:20 +0200

    add notify after product create

--- src/components/containers/AddProductFormContainer.vue
+++ src/components/containers/AddProductFormContainer.vue
@@ -1,29 +1,44 @@
 <template>
-<add-product-form @submit-form="onFormSubmit"></add-product-form>
+  <add-product-form @submit-form="onFormSubmit"></add-product-form>
+  <v-notification :is-show="isShowNotification">Товар успешно добавлен</v-notification>
 </template>
 
 <script>
+import { ref } from 'vue';
 import { useStore } from 'vuex';
 
 import AddProductForm from '../AddProductForm/AddProductForm.vue';
+import VNotification from '../VNotification.vue';
 
 export default {
   name: 'AddProductFormContainer',
   components: {
     AddProductForm,
+    VNotification,
   },
 
   setup() {
     const store = useStore();
-    const onFormSubmit = (product) => {
+    const isShowNotification = ref(false);
+
+    const showNotification = () => {
+      isShowNotification.value = true;
+      const timer = setTimeout(() => {
+        isShowNotification.value = false;
+        clearTimeout(timer);
+      }, 2000);
+    };
+    const onFormSubmit = async (product) => {
       const productWithId = {
         id: String(Math.random()),
         ...product,
       };
-      store.dispatch('ProductStore/addProduct', productWithId);
+      await store.dispatch('ProductStore/addProduct', productWithId);
+      showNotification();
     };
     return {
       onFormSubmit,
+      isShowNotification,
     };
   },
 };
