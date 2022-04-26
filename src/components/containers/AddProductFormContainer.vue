ba1f783bd705da18322d9b709d01d4db119df4c4
Vishnik <Taras.Vishnik@enterra-inc.com>
2022-04-26 03:24:00 +0200

    add AddProductFormContainer component

---
+++ src/components/containers/AddProductFormContainer.vue
@@ -0,0 +1,34 @@
+<template>
+<add-product-form @submit-form="onFormSubmit"></add-product-form>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+import AddProductForm from '../AddProductForm/AddProductForm.vue';
+
+export default {
+  name: 'AddProductFormContainer',
+  components: {
+    AddProductForm,
+  },
+
+  setup() {
+    const store = useStore();
+    const onFormSubmit = (product) => {
+      const productWithId = {
+        id: String(Math.random()),
+        ...product,
+      };
+      store.dispatch('ProductStore/addProduct', productWithId);
+    };
+    return {
+      onFormSubmit,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
